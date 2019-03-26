07b3ade13083751557acab74dd0771f2af438fda
Roberto Orozco <issc.roberto@outlook.com>
2019-03-26 16:08:39 -0600

    UI adjustments and fixes

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,6 @@ import 'dart:math' as math;
 
 void main() => runApp(MaterialApp(
       home: OneTimerApp(),
-      theme: ThemeData(
-          canvasColor: Colors.blueGrey,
-          iconTheme: IconThemeData(color: Colors.white),
-          accentColor: Colors.pinkAccent,
-          brightness: Brightness.dark),
     ));
 
 class OneTimerApp extends StatefulWidget {
@@ -36,7 +31,7 @@ class OneTimerState extends State<OneTimerApp> with TickerProviderStateMixin {
     ThemeData themeData = Theme.of(context);
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
             Expanded(
@@ -53,29 +48,21 @@ class OneTimerState extends State<OneTimerApp> with TickerProviderStateMixin {
                             return CustomPaint(
                                 painter: TimerPainter(
                                     animation: controller,
-                                    backgroundColor: Colors.red,
+                                    backgroundColor: Colors.grey,
                                     color: themeData.indicatorColor));
                           },
                         ),
                       ),
                       Align(
                           alignment: FractionalOffset.center,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Text("One Timer",
-                                  style: themeData.textTheme.subhead),
-                              AnimatedBuilder(
-                                animation: controller,
-                                builder: (BuildContext context, Widget child) {
-                                  return Text(
-                                    timerString,
-                                    style: themeData.textTheme.display4,
-                                  );
-                                },
-                              ),
-                            ],
+                          child: AnimatedBuilder(
+                            animation: controller,
+                            builder: (BuildContext context, Widget child) {
+                              return Text(
+                                timerString,
+                                style: themeData.textTheme.display4,
+                              );
+                            },
                           ))
                     ],
                   ),
@@ -83,7 +70,7 @@ class OneTimerState extends State<OneTimerApp> with TickerProviderStateMixin {
               ),
             ),
             Container(
-              margin: EdgeInsets.all(8.0),
+              margin: EdgeInsets.all(16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
@@ -98,7 +85,9 @@ class OneTimerState extends State<OneTimerApp> with TickerProviderStateMixin {
                     ),
                     onPressed: () {
                       if (controller.isAnimating) {
-                        controller.stop();
+                        setState(() {
+                          controller.stop();
+                        });
                       } else {
                         controller.reverse(
                             from: controller.value == 0.0
